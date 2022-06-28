9559bf460a967a2ee1d700ee93980183f517ca1f
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-06-29 00:01:14 +0200

    SSSS

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={HibernateJpaAutoConfiguration.class})
 @ServletComponentScan
 @EnableJpaRepositories
 public class DemoApplication {
--- src/main/java/com/example/demo/entities/Tidslinje.java
+++ src/main/java/com/example/demo/entities/Tidslinje.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
-@Table(name="\"Tidslinje\"", schema = "\"schemaTest\"")
+@Table(name="Tidslinje", schema = "schemaTest")
 public class Tidslinje  {
 
     @Id
