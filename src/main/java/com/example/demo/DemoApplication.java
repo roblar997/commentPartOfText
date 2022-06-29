37d86e76fdb09799c2022e0b42c4ecea65e6feb6
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-06-29 10:48:05 +0200

    ggg

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @ServletComponentScan
 @EnableJpaRepositories
+
 public class DemoApplication {
 
     public static void main(String[] args) {
