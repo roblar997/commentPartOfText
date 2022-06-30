5ab5ad99f57ce29e92c79200ff32a4f466c964d2
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-06-30 22:29:22 +0200

    ddd

--- src/main/java/com/example/demo/entities/textToComment.java
+++ src/main/java/com/example/demo/entities/textToComment.java
@@ -21,7 +21,7 @@ public class textToComment {
     @Column(name = "\"text\"")
     private String text;
 
-     @OneToMany(mappedBy = "id")
+     @OneToMany(mappedBy = "\"id\"")
      @JsonIgnore
      Collection<Tidslinje> tidslinjer;
 }
