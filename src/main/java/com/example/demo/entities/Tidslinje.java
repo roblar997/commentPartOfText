a5b9de9073d41b5be4607f84f47eb48b12e4e8aa
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-06-30 23:43:16 +0200

    eee

--- src/main/java/com/example/demo/entities/Tidslinje.java
+++ src/main/java/com/example/demo/entities/Tidslinje.java
@@ -1,5 +1,6 @@
 package com.example.demo.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.NotFound;
 import org.hibernate.annotations.NotFoundAction;
 
@@ -49,6 +50,7 @@ public class Tidslinje  implements Serializable {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "\"texttocommentid\"",updatable = false,insertable = false)
+    @JsonIgnore
     textToComment texttocommentfield;
 
     public Tidslinje(Integer id, String user, Long timestampCreated, Long timestampChanged, Integer start, Integer end, String text, Boolean like, Boolean dislike, Boolean isdeleted, Integer texttocommentid) {
