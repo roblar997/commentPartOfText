622a5e360f2aca73456259fa7d72e30e1fd11e4a
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-06-30 18:36:19 +0200

    ddd

---
+++ src/main/java/com/example/demo/entities/textToComment.java
@@ -0,0 +1,22 @@
+package com.example.demo.entities;
+
+import javax.persistence.*;
+
+import java.util.List;
+
+import static javax.persistence.GenerationType.IDENTITY;
+@Entity
+//@Table(name="\"texttocomment\"", schema = "\"schematest\"")
+public class textToComment {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+   // @Column(name = "\"id\"")
+    private Integer id;
+    //@Column(name = "\"text\"")
+    private String text;
+
+    @OneToMany(mappedBy = "Tidslinje", fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    List<Tidslinje> tidslinjer;
+}
