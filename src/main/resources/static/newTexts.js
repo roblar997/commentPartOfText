30bbfed4cf42ce28999d75a5094ee71e7de2c018
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-07-01 21:38:53 +0200

    sss

---
+++ src/main/resources/static/newTexts.js
@@ -0,0 +1,43 @@
+(function(){
+    let texts = [];
+
+    async function addPNewText(text) {
+
+        await $.post({
+            url: '/newTextServlet',
+            data: JSON.stringify({ "remoteMethod": "addText", "text": text}),
+            contentType: "application/json; charset=utf-8"
+        }).done((res) => {
+
+        }).promise();
+
+    }
+    async function getTextsFromServer() {
+
+        await $.post({
+            url: '/newTextServlet',
+            data: JSON.stringify({ "remoteMethod": "getTexts" }),
+            contentType: "application/json; charset=utf-8"
+        }).done((res) => {
+            for (const [key, value] of Object.entries(res)){
+
+                texts = res;
+            }
+        }).promise();
+
+    }
+    async  function  getPTexts(){
+        await getTextsFromServer();
+        return texts;
+    }
+
+    return {
+        getTexts: function(){
+            return getPTexts();
+        },
+        addNewText: function (text){
+            return addPNewText(text)
+        }
+
+    }
+})();
