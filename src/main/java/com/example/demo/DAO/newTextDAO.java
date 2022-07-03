aee05465a31aee6247b626e724d997bcd65f85ee
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-07-03 16:37:09 +0200

    sss

--- src/main/java/com/example/demo/DAO/newTextDAO.java
+++ src/main/java/com/example/demo/DAO/newTextDAO.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Component
 @Stateless
@@ -13,5 +14,18 @@ public class newTextDAO {
     @PersistenceContext
     @Autowired
     private EntityManager em;
+
+    public String addText(String title, String text){
+
+        return "OK";
+    }
+    public List<String> getTitles(){
+        return  null;
+    }
+
+    public String getText(String title){
+
+        return "...";
+    }
 
 }
