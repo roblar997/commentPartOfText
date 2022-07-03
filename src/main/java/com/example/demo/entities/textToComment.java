bbace1055b894222b6a5d743f39635859a70c68a
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-07-03 19:33:16 +0200

    sss

--- src/main/java/com/example/demo/entities/textToComment.java
+++ src/main/java/com/example/demo/entities/textToComment.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 
 import static javax.persistence.GenerationType.IDENTITY;
@@ -19,6 +20,54 @@ public class textToComment implements Serializable {
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "\"id\"")
     private Integer id;
+
+    @Override
+    public String toString() {
+        return "textToComment{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", title='" + title + '\'' +
+                ", tidslinjer=" + tidslinjer +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof textToComment)) return false;
+        textToComment that = (textToComment) o;
+        return Objects.equals(id, that.id) && Objects.equals(text, that.text) && Objects.equals(title, that.title) && Objects.equals(tidslinjer, that.tidslinjer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, text, title, tidslinjer);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     @Column(name = "\"text\"")
     private String text;
     @Column(name = "\"title\"")
