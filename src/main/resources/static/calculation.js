edabeaad6a5225e33ae217c50d55716e79b92d85
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-07-05 20:23:13 +0200

    ff

--- src/main/resources/static/calculation.js
+++ src/main/resources/static/calculation.js
@@ -84,8 +84,8 @@ var timeLineModule = (function(){
             this.fenwFeatureTree = new FenwFeatureTree(parseInt($("#textToComment").val().length))
             //this.timestamp = res.timestamp
 
-            for (const [key, value] of Object.entries(res)){
-                timeLines.push(value)
+            for (let val in res){
+                timeLines.push(val)
             }
         }).promise();
 
